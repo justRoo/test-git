ce26803b667cb2d9c14953116fcd2f9bbd7ffb60
justRoo <13410429040@163.com>
2017-11-03 16:25:39 +0800

    map  跟  forEach 区别

--- js/comment.js
+++ js/comment.js
@@ -4,3 +4,13 @@ window.onload = function () {
 function onStart(){
     console.log('hello world!')
 }
+let arr = [1,2,3];
+let a = [];
+let b = [];
+a.push.call(a,arr.forEach(function(x,y){
+  return x*y
+}));
+b.push.call(b,arr.map(function(x,y){
+  return x*y;
+}))
+console.log(a,b);
